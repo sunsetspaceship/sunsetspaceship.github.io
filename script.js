ba4fea2c8817909072ddd4439ab2f963cc120b76
sunsetspaceship <151386949+sunsetspaceship@users.noreply.github.com>
2023-11-23 18:09:13 -0300

    New

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
     // Function to add item to the grocery list
     function addItem() {
         // Get the input element by its ID
-        var inputElement = document.getElementById("addItemInput");
+        const inputElement = document.getElementById("addItemInput");
 
         // Get the value (text) from the input
-        var inputValue = inputElement.value;
+        const inputValue = inputElement.value;
 
         // Clear the input field
         inputElement.value = "";
 
         // Get the ul element by its ID
-        var ulElement = document.getElementById("groceryList");
+        const ulElement = document.getElementById("groceryList");
 
         // Create a new li element
-        var liElement = document.createElement("li");
+        const liElement = document.createElement("li");
 
         // Set the text content of the li element to the entered value
         liElement.textContent = inputValue;
@@ -24,6 +24,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // Add event listener to the "Save" button
-    var saveButton = document.querySelector("button");
+    const saveButton = document.querySelector("button");
     saveButton.addEventListener("click", addItem);
 });
