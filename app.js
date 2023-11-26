97eade13940c26797cb4f068b19f0e276d806de2
sunsetspaceship <151386949+sunsetspaceship@users.noreply.github.com>
2023-11-25 22:43:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const input = document.getElementById("input");
 const list = document.getElementById("list");
 
@@ -7,17 +8,29 @@ function addItem() {
         li.innerHTML = input.value;
         list.appendChild(li);
         let span = document.createElement("span");
-        span.innerHTML = "&#x1F5D1;";
+        span.innerHTML = "&#128465;";
         li.appendChild(span);
     }
     input.value = "";
+    saveData();
 }
 
 list.addEventListener("click" , function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData()
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData()
     }
 }, false);
+
+function saveData() {
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function showData() {
+    list.innerHTML = localStorage.getItem("data")
+}
+showData();
